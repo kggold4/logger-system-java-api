863681b954e05d1d7ff7bdf231150f82a85e8336
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-07-26 19:07:56 +0300

    add create, delete and pring all users functons

--- src/Logger.java
+++ src/Logger.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Logger {
 
@@ -32,8 +33,8 @@ public class Logger {
     public boolean connect(int id, String password) {
         try {
             try(Connection con  = DriverManager.getConnection("jdbc:mysql://localhost:3306/logger", "root", "kfir1144")) {
-                Statement stmt = con.createStatement();
-                String query = "SELECT id, firstname, lastname, email, reg_date  from users WHERE id=? AND pwd=?";
+
+                String query = "SELECT id, firstname, lastname, email, reg_date from users WHERE id=? AND pwd=?";
                 try(PreparedStatement pstmt = con.prepareStatement(query)) {
 
                     pstmt.setInt(1, id);
@@ -59,6 +60,93 @@ public class Logger {
     public void disconnect() { this.user = null; this.connected = false; }
 
     public boolean check() { return this.connected; }
+
+    public String create(String firstName, String lastName, String email, String password) {
+        int user_id;
+        try {
+            try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/logger", "root", "kfir1144")) {
+                String query = "SELECT MAX(id) FROM users";
+                try(PreparedStatement pstmt = con.prepareStatement(query)) {
+                    ResultSet rs = pstmt.executeQuery();
+                    rs.next();
+                    user_id = Integer.parseInt(rs.getString(1));
+                }
+            }
+        } catch(Exception ex) { ex.printStackTrace(); return "Error"; }
+        user_id++;
+        try {
+            try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/logger", "root", "kfir1144")) {
+
+                String query = "INSERT INTO users VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";
+                try(PreparedStatement pstmt = con.prepareStatement(query)) {
+
+                    pstmt.setInt(1, user_id);
+                    pstmt.setString(2, firstName);
+                    pstmt.setString(3, lastName);
+                    pstmt.setString(4, email);
+                    pstmt.setString(5, password);
+                    pstmt.executeUpdate();
+                }
+            }
+        } catch(Exception ex) { ex.printStackTrace(); return "Error"; }
+        return "" + user_id;
+    }
+
+    public boolean delete(int id, String password) {
+        if(id == 100001) return false; // cannot delete admin
+        boolean current_connected = connect(id, password);
+        if(!current_connected) return false;
+
+        try {
+            try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/logger", "root", "kfir1144")) {
+                String query = "DELETE FROM users WHERE id=? AND pwd=?";
+                try(PreparedStatement pstmt = con.prepareStatement(query)) {
+                    pstmt.setInt(1, id);
+                    pstmt.setString(2, password);
+                    pstmt.executeUpdate();
+                }
+            }
+        } catch(Exception ex) { ex.printStackTrace(); return false; }
+        return true;
+    }
+
+    public String[][] printAllUsers(int id, String password) throws SQLException {
+        if(id != 100001) throw new IllegalArgumentException("Must Connect to Admin User");
+        boolean current_connected = connect(id, password);
+        if(!current_connected) throw new IllegalArgumentException("Wrong Password");
+
+        try {
+            try(Connection con  = DriverManager.getConnection("jdbc:mysql://localhost:3306/logger", "root", "kfir1144")) {
+
+                String query = "SELECT id, firstname, lastname, email, reg_date from users";
+                try(PreparedStatement pstmt = con.prepareStatement(query)) {
+
+                    ResultSet rs = pstmt.executeQuery();
+
+                    int numOfColumns = rs.getMetaData().getColumnCount();
+                    ArrayList<ArrayList<String>> result_al = new ArrayList<>();
+                    while(rs.next()){
+                        ArrayList<String> row = new ArrayList<>();
+                        for(int col = 1; col <= numOfColumns; col++){
+                            row.add(rs.getString(col));
+                        }
+                        result_al.add(row);
+                    }
+
+                    String[][] result = new String[numOfColumns][result_al.size()];
+                    for(int i = 0; i < numOfColumns; i++) {
+                        for(int j = 0; j < result_al.size(); j++) {
+                            result[i][j] = result_al.get(i).get(j);
+                        }
+                    }
+
+                    disconnect();
+                    return result;
+                }
+            }
+        } catch(Exception ex) { ex.printStackTrace(); }
+        throw new SQLException("Something Went Wrong, Please Try Again");
+    }
 
     public String getUserName() { return ((this.connected) ? this.user.firstName + this.user.lastName : ""); }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import com.mysql.cj.log.Log;
+
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Logger logger = new Logger();
         Scanner number_scanner = new Scanner(System.in);
         Scanner text_scanner = new Scanner(System.in);
@@ -12,6 +15,9 @@ public class Main {
             System.out.println("2) Log-in");
             System.out.println("3) Log-out");
             System.out.println("4) User Details");
+            System.out.println("5) Create User");
+            System.out.println("6) Delete User");
+            System.out.println("7) Print All Users");
             int op = number_scanner.nextInt();
             switch (op) {
                 case 0:
@@ -48,6 +54,60 @@ public class Main {
                 case 4:
                     if(logger.check()) System.out.println(logger);
                     else System.out.println("\n\tNo User is connected\n");
+                    break;
+                case 5:
+                    System.out.println("Please enter First Name:");
+                    String new_firstName = text_scanner.nextLine();
+                    System.out.println("Please enter Last Name:");
+                    String new_lastName = text_scanner.nextLine();
+                    System.out.println("Please enter Email:");
+                    String new_email = text_scanner.nextLine();
+                    System.out.println("Please enter Password:");
+                    String new_password = text_scanner.nextLine();
+                    String res = logger.create(new_firstName, new_lastName, new_email, new_password);
+                    if(res.equals("AlreadyExists")) {
+                        System.out.println("\n\tUser ID is Already Exists, Please Try Again\n");
+                    } else if(res.equals("ERROR")) {
+                        System.out.println("\n\tSomething Went Wrong, Please Try Again\n");
+                    } else {
+                        System.out.println("\n\tUser Created, User ID is: " + res + "\n");
+                    }
+                    break;
+                case 6:
+                    if(logger.check()) logger.disconnect();
+
+                    // getting id
+                    System.out.println("\n\tPlease Enter User ID:\n");
+                    int delete_id = number_scanner.nextInt();
+
+                    // getting password
+                    System.out.println("\n\tPlease Enter User Password:\n");
+                    String delete_password = text_scanner.nextLine();
+                    boolean deleted = logger.delete(delete_id, delete_password);
+                    if(deleted) {
+                        System.out.println("\n\tUser Deleted Successfully\n");
+                    } else {
+                        System.out.println("\n\tSomething Went Wrong, Please Try Again\n");
+                    }
+                    break;
+                case 7:
+                    if(logger.check()) logger.disconnect();
+
+                    // getting id
+                    System.out.println("\n\tPlease Enter Admin User ID:\n");
+                    int admin_id = number_scanner.nextInt();
+
+                    // getting password
+                    System.out.println("\n\tPlease Enter Admin User Password:\n");
+                    String admin_password = text_scanner.nextLine();
+
+                    String[][] users = logger.printAllUsers(admin_id, admin_password);
+                    for(int i = 0; i < users.length; i++) {
+                        for(int j = 0; j < users[i].length; j++) {
+                            System.out.print(users[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
                     break;
                 default:
                     continue;
